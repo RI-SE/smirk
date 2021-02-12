30bcd48d3547745c72c0b31df2565f9686a0163b
Kasper Socha <kaspersocha@gmail.com>
2021-02-12 14:00:48 +0100

    Add training of SSD MobileNet v2 320x320

--- pedestrian-detector/train.py
+++ pedestrian-detector/train.py
@@ -10,7 +10,12 @@ MODELS = {
         "origin_name": "efficientdet_d0_coco17_tpu-32",
         "name": "efficient-det-d0",
         "url": "http://download.tensorflow.org/models/object_detection/tf2/20200711/efficientdet_d0_coco17_tpu-32.tar.gz",
-    }
+    },
+    "ssd_mobilenet_320": {
+        "origin_name": "ssd_mobilenet_v2_320x320_coco17_tpu-8",
+        "name": "ssd_mobilenet_320",
+        "url": "http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_mobilenet_v2_320x320_coco17_tpu-8.tar.gz",
+    },
 }
 
 TRAINED_BASE_DIR = Path("./trained")
