59e72f1b106afefb4a626f4be3569a73aeb89fb2
Kasper Socha <kasper.socha@ri.se>
2022-01-30 21:48:53 +0100

    Fix NoopDetector incorrect return type

--- src/smirk/pedestrian_detector/noop_detector.py
+++ src/smirk/pedestrian_detector/noop_detector.py
@@ -1,8 +1,13 @@
+from typing import List
+
 import numpy as np
 
-from smirk.pedestrian_detector.pedestrian_detector import PedestrianDetector
+from smirk.pedestrian_detector.pedestrian_detector import (
+    BoundingBox,
+    PedestrianDetector,
+)
 
 
 class NoopDetector(PedestrianDetector):
-    def detect_pedestrians(self, camera_frame: np.ndarray) -> bool:
-        return False
+    def detect_pedestrians(self, camera_frame: np.ndarray) -> List[BoundingBox]:
+        return []
