eff11aa74493e65248e1f5ecb6eb7f9cd2727b7c
Kasper Socha <kasper.socha@ri.se>
2022-06-20 09:42:26 +0200

    Add GPL header to all source code files
    See: #21

--- src/tests/system/generate_all_pairs.py
+++ src/tests/system/generate_all_pairs.py
@@ -1,3 +1,20 @@
+#
+# SMIRK
+# Copyright (C) 2021-2022 RISE Research Institutes of Sweden AB
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 """
 TODO: This is all tigthly and implicitly coupled to the simple_aeb_scene! It makes a number of implicit assumptions about angles, object positions, coordinate system etc. Try to at least make that more explicit.
 """
